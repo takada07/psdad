32d8a02c01d7ad1e55f6f992c6ec12da240095f9
Radoslav Strobl <spacok@gmail.com>
2014-10-25 18:23:21 +0200

    create server file

---
+++ include/server.h
@@ -0,0 +1,6 @@
+#ifndef __SERVER_H__
+#define __SERVER_H__
+
+int init_server(char *dev);
+
+#endif //__SERVER_H__
---
+++ src/server.c
@@ -0,0 +1,12 @@
+/* server.c -- simple server for managing UP request for hosts
+
+Copyright (c) <2014> Radoslav Strobl <spacok@gmail.com>
+
+Part of psdad project distributed under MIT Licence (see LICENSE.txt)
+
+*/
+
+
+int init_server(char *dev)
+{
+}
