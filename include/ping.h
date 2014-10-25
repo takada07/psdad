8009c4eb014d1047e03a7f83d6e4c073c9cb2499
takada07 <takada@gmx.com>
2014-10-25 17:12:38 +0200

    Create ping.h

---
+++ include/ping.h
@@ -0,0 +1,15 @@
+/* ping.h -- header for ping.c
+
+Copyright (c) <2014> Ernest Takada <takada@gmx.com>
+
+Part of psdad project distributed under MIT Licence (see LICENSE.txt)
+
+*/
+
+
+#ifndef __PING_H__
+#define __PING_H__
+
+int ping(const struct in_addr *dest_ip);
+
+#endif //__PING_H__
