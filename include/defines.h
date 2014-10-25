14f0c8fe56295392913ca2a91a22206b6d3a6142
takada07 <takada@gmx.com>
2014-10-25 17:11:15 +0200

    Create defines.h

---
+++ include/defines.h
@@ -0,0 +1,29 @@
+/* defines.h -- global definitions
+
+Copyright (c) <2014> Ernest Takada <takada@gmx.com>
+
+Part of psdad project distributed under MIT Licence (see LICENSE.txt)
+
+*/
+
+
+#ifndef __DEFINES_H__
+#define __DEFINES_H__
+
+//#define DEBUG
+//#undef DEBUG
+/* default snap length (maximum bytes per packet to capture) */
+#define SNAP_LEN 1518
+/* ethernet headers are always exactly 14 bytes [1] */
+#define SIZE_ETHERNET 14
+
+#define IP_HL(ip)               (((ip)->ip_vhl) & 0x0f)
+#define IP_V(ip)                (((ip)->ip_vhl) >> 4)
+
+#ifdef DEBUG_ALL
+    #define debug_printf(...) fprintf( stderr, __VA_ARGS__ ); 
+#else
+    #define debug_printf(...) 
+#endif
+
+#endif //__DEFINES_H__
