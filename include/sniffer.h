edf4a2a8670ea73cbfab5ee21493bf64b22179dd
takada07 <takada@gmx.com>
2014-10-25 17:14:11 +0200

    Create sniffer.h

---
+++ include/sniffer.h
@@ -0,0 +1,15 @@
+/* sniffer.h -- header file for sniffer.c
+
+Copyright (c) <2014> Ernest Takada <takada@gmx.com>
+
+Part of psdad project distributed under MIT Licence (see LICENSE.txt)
+
+*/
+
+#ifndef __SNIFFER_H__
+#define __SNIFFER_H__
+
+int init_sniffer(char *conf_dev);
+int start_loop();
+
+#endif
