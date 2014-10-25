26744d63d946b2bc72c3c5244788712b0171ede6
takada07 <takada@gmx.com>
2014-10-25 17:10:41 +0200

    Create config.h

---
+++ include/config.h
@@ -0,0 +1,38 @@
+/* config.h -- header for config.c
+
+Copyright (c) <2014> Ernest Takada <takada@gmx.com>
+
+Part of psdad project distributed under MIT Licence (see LICENSE.txt)
+
+*/
+
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+#include <netinet/in.h>
+#include <net/ethernet.h>
+
+struct cfgData {
+    char section[30];
+    int id;
+    struct in_addr ip;
+    unsigned char ether[ETHER_ADDR_LEN];
+    unsigned int *tcpportarray;
+    size_t tcpportarraylen;
+    unsigned int *udpportarray;
+    size_t udpportarraylen;
+    char status;
+    int *sockets;
+    int *acceptsockets;
+    u_char *forwardpacket;
+    int fwpacketlen;
+};
+
+int read_config(char *cfgName);
+struct cfgData *get_cfg(struct in_addr ipaddr);
+void destroy_config();
+int set_ifaces(const char *dev);
+int ifdown(const char *dev, struct cfgData *data);
+int ifup(const char *dev, struct cfgData *data);
+
+#endif
